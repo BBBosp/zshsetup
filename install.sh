8a49f9cb4ec5c863e91eec2a03a0fd8d102433c5
BBBosp <atkinson.unt@gmail.com>
2020-01-04 20:52:27 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,34 +6,44 @@
 cd ~
 
 #install basic dependencies
+echo "installing basic dependencies"
 sudo yum install -y zsh git curl util-linux-user nano
 
 #install ohmyzsh
+echo "copying .oh-my-zsh file" 
 cp -r ~/zshsetup/.oh-my-zsh ~/.oh-my-zsh
 
 #install powerlevel9k
+echo "cloning theme Powerlevel9k"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 #install personal preferredd plugins
+echo "grabbing my favorite plugins"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 #back up .zshrc
+echo "if .zshrc exists back it up!"
 cp ~/.zshrc ~/.zshrc.orig
 
 #bring in personal louadout
+echo "bring in preferred config"
 cp ~/zshsetup/.zshrc ~/.zshrc
 
-#clone nerdfonts repo... This will take a while, it's a big repo, and seeds slowly. 
+#clone nerdfonts repo... This will take a while, it's a big repo, and seeds slowly.
+echo "getting nerdfonts, this grabs all because I'm lazy, just wait for it."
 git clone https://github.com/ryanoasis/nerd-fonts
 
 #run install script for nerd fonts
+echo "installing nerdfonts"
 ./nerd-fonts/install.sh
 
 #make zsh default shell
+echo "making zsh default shell"
 chsh -s /bin/zsh
 
 #reload .zshrc
+echo "reload .zshrc"
 . ~/.zshrc
 
 zsh
