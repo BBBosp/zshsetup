4f20bfa8c596efb96e651cfbb0ec1b16bac5d58f
bosp <atkinson.unt@gmail.com>
2020-01-04 20:31:55 -0600

    updated install.sh

--- install.sh
+++ install.sh
@@ -12,11 +12,11 @@ sudo yum install -y zsh git curl util-linux-user
 cp -r ~/zshsetup/.oh-my-zsh ~/.oh-my-zsh
 
 #install powerlevel9k
-#git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 #install personal preferredd plugins
-#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 #back up .zshrc
 cp ~/.zshrc ~/.zshrc.orig
@@ -36,6 +36,6 @@ chsh -s /bin/zsh
 #reload .zshrc
 . ~/.zshrc
 
-
+zsh
 
 #now you need to go change the font in preferences in the terminal GUI. I haven't found ou where to change it in the terminal yet.
