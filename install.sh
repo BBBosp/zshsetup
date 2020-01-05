0d29268d0bebe9138f7cd6ef7e79f484e1a7e660
bosp <atkinson.unt@gmail.com>
2020-01-04 19:48:08 -0600

    updated install.sh

--- install.sh
+++ install.sh
@@ -6,17 +6,23 @@
 cd ~
 
 #install basic dependencies
-sudo yum install -y zsh git curl
+sudo yum install -y zsh git curl util-linux-user
 
 #install ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp -r ~/zshsetup/.oh-my-zsh ~/.oh-my-zsh
 
 #install powerlevel9k
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+#git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 #install personal preferredd plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#back up .zshrc
+cp ~/.zshrc ~/.zshrc.orig
+
+#bring in personal louadout
+cp ~/zshsetup/.zshrc ~/.zshrc
 
 #clone nerdfonts repo... This will take a while, it's a big repo, and seeds slowly. 
 git clone https://github.com/ryanoasis/nerd-fonts
@@ -24,17 +30,12 @@ git clone https://github.com/ryanoasis/nerd-fonts
 #run install script for nerd fonts
 ./nerd-fonts/install.sh
 
-#back up .zshrc
-cp .zshrc .zshrc.original
-
-#bring in personal louadout
-cp zshsetup/.zshrc .zshrc
+#make zsh default shell
+chsh -s /bin/zsh
 
 #reload .zshrc
 . ~/.zshrc
 
-#make zsh default shell
-chsh -s $(which zsh)
 
 
 #now you need to go change the font in preferences in the terminal GUI. I haven't found ou where to change it in the terminal yet.
